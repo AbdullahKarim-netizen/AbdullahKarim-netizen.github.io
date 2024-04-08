1716a4120d1ac6d71a6f55be8a950e100961ea37
AbdullahKarim-netizen <blueblashwater.123@gmail.com>
2024-04-09 01:07:18 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,65 @@
+var tombolvar1 = document.getElementById('tombol-suka');
+
+function Toggle1(){
+    if (tombolvar1.style.color == "red") {
+        tombolvar1.style.color = "grey"
+    }
+    else {
+        tombolvar1.style.color = "red"
+    }
+}
+
+var tombolvar2 = document.getElementById('tombol-simpan');
+
+function Toggle2(){
+    if (tombolvar2.style.color == "black") {
+        tombolvar2.style.color = "grey"
+    }
+    else {
+        tombolvar2.style.color = "black"
+    }
+}
+// 2
+var tombolvar3 = document.getElementById('tombol-suka-kedua');
+
+function Toggle5(){
+    if (tombolvar3.style.color == "red") {
+        tombolvar3.style.color = "grey"
+    }
+    else {
+        tombolvar3.style.color = "red"
+    }
+}
+
+var tombolvar4 = document.getElementById('tombol-simpan-kedua');
+
+function Toggle6(){
+    if (tombolvar4.style.color == "black") {
+        tombolvar4.style.color = "grey"
+    }
+    else {
+        tombolvar4.style.color = "black"
+    }
+}
+//  3
+var tombolvar5 = document.getElementById('tombol-suka-ketiga');
+
+function Toggle7(){
+    if (tombolvar5.style.color == "red") {
+        tombolvar5.style.color = "grey"
+    }
+    else {
+        tombolvar5.style.color = "red"
+    }
+}
+
+var tombolvar6 = document.getElementById('tombol-simpan-ketiga');
+
+function Toggle8(){
+    if (tombolvar6.style.color == "black") {
+        tombolvar6.style.color = "grey"
+    }
+    else {
+        tombolvar6.style.color = "black"
+    }
+}
